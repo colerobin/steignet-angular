ec8d2b747be549e244eba79a404a0966a9ddf2e5
Johan <goldenking0412@gmail.com>
2018-09-03 09:20:05 -0400

    show master inventory data from back-end response

--- src/app/Inventory/inventory-page/inventory-page.component.ts
+++ src/app/Inventory/inventory-page/inventory-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { inventory_tables, inventory_charts } from '../../../dummy/inventory';
 
+import { Globals } from '../../globals';
+
 declare var $: any;
 
 @Component({
@@ -14,9 +16,35 @@ export class InventoryPageComponent implements OnInit {
   inventory_tables: any[];
   inventory_charts: any[];
 
-  constructor() { }
+  isLoaded = true;
+
+  private backend_url: string;
+
+  constructor(private globals: Globals) { 
+    this.backend_url = globals.backend_url;
+  }
 
   ngOnInit() {
+    var master;
+    $.ajax({
+      url: this.backend_url+'/master-inventory',
+      method: 'get',
+      success: function(result){
+        master = result.split("\n");
+        for (var i = master.length - 1; i >= 0; i--) {
+          master[i] = master[i].split(",");
+        }
+        var temp = inventory_tables[0]['data'][1][2];
+        inventory_tables[0]['data'] = master;
+        this.isLoaded = true;
+        Object.assign(this, {
+          inventory_tables,
+          inventory_charts
+        })
+        $("#sortablea").sortable();
+        $("#sortableb").sortable();
+      }
+    });
     Object.assign(this, {
       inventory_tables,
       inventory_charts
